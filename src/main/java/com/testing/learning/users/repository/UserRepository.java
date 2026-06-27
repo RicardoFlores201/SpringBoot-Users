377c616dbfce89daa47fec41324ef51028cb50c8
richardram233 <richardram233@gmail.com>
2026-06-27 14:49:21 -0600

    Implementación de UserRepository

--- src/main/java/com/testing/learning/users/repository/UserRepository.java
+++ src/main/java/com/testing/learning/users/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.testing.learning.users.repository;
 
-public class UserRepository {
+import com.testing.learning.users.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//El String en JpaRepository<UserEntity, String> es el tipo de dato del @Id
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 }
