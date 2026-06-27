c7c5a91b91b1585e171faf1e5eab434fa7662b0d
richardram233 <richardram233@gmail.com>
2026-06-27 14:45:04 -0600

    desarrollo de UserDTO

--- src/main/java/com/testing/learning/users/dto/UserDTO.java
+++ src/main/java/com/testing/learning/users/dto/UserDTO.java
@@ -2,9 +2,20 @@ package com.testing.learning.users.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-public class UserDTO {
+@NoArgsConstructor
+public class UserDTO { //Solo se expone lo que el cliente necesita ver
 
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String country;
+    private String website;
+    private String username;
+    private Integer followers;
+    private Integer following;
 }
